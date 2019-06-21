41c30f7cb405ad2f1a05251189cf63cc50113160
David Conner <agilebluefox@gmail.com>
2019-06-20 21:17:49 -0400

    Improve code layout

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-$(document).ready( function() {
+$(document).ready(function () {
 
     // Create an array of objects for the questions.
-    var questions = [
-        {
+    var questions = [{
             questNum: 1,
             questText: 'Which player on the ATP Tour has won the most Grand Slam titles?',
             mysteryImg: 'images/mystery-federer.png',
             actualImg: 'images/federer.png',
-            choices: ['Pete Sampras', 'Rafael Nadal', 'Roger Federer', 'John McEnroe'],
+            choices: ['Pete Sampras', 'Rafael Nadal',
+                'Roger Federer', 'John McEnroe'
+            ],
             correct: 2,
             feedback: 'Roger Federer has won 17 Grand Slam titles including 7 Wimbledon, 5 US Open, 1 French Open, and 4 Australian Open titles.'
         },
@@ -17,7 +18,9 @@ $(document).ready( function() {
             questText: 'This player has won the French Open a whopping nine times over a ten year period!',
             mysteryImg: 'images/mystery-nadal.png',
             actualImg: 'images/nadal.png',
-            choices: ['Roger Federer', 'Rafael Nadal', 'Novak Djokovic', 'John Isner'],
+            choices: ['Roger Federer', 'Rafael Nadal',
+                'Novak Djokovic', 'John Isner'
+            ],
             correct: 1,
             feedback: 'Rafael Nadal is considered to be "The King of Clay" and one of the greatest players ever.'
         },
@@ -27,7 +30,9 @@ $(document).ready( function() {
             questText: 'Which of these players won the men\'s singles championship at the 2012 Olympic Games?',
             mysteryImg: 'images/mystery-murray.png',
             actualImg: 'images/murray.png',
-            choices: ['Andy Murray', 'Novak Djokovic', 'Pete Sampras', 'Andy Roddick'],
+            choices: ['Andy Murray', 'Novak Djokovic',
+                'Pete Sampras', 'Andy Roddick'
+            ],
             correct: 0,
             feedback: 'Andy Murray is the first British singles champion in over 100 years.'
         },
@@ -37,7 +42,9 @@ $(document).ready( function() {
             questText: 'Who is the only player to have beaten both Roger Federer and Rafael Nadal in all four Grand Slam Events?',
             mysteryImg: 'images/mystery-djokovic.png',
             actualImg: 'images/djokovic.png',
-            choices: ['Andre Agassi', 'Andy Roddick', 'Novak Djokovic', 'Pete Sampras'],
+            choices: ['Andre Agassi', 'Andy Roddick',
+                'Novak Djokovic', 'Pete Sampras'
+            ],
             correct: 2,
             feedback: 'Novak Djokovic is currently ranked number 1 in the world on the ATP Tour.'
         },
@@ -47,7 +54,9 @@ $(document).ready( function() {
             questText: 'This player won the longest men\'s singles match ever to be played at Wimbledon.',
             mysteryImg: 'images/mystery-isner.png',
             actualImg: 'images/isner.png',
-            choices: ['Rafael Nadal', 'John McEnroe', 'Pete Sampras', 'John Isner'],
+            choices: ['Rafael Nadal', 'John McEnroe',
+                'Pete Sampras', 'John Isner'
+            ],
             correct: 3,
             feedback: 'John Isner defeated Nicolaus Mahut in 11 hours and 5 minutes. The match was played over a three day period since there are no court lights at Wimbledon.'
         }
@@ -68,7 +77,7 @@ $(document).ready( function() {
         updateQuestion();
     }
 
-    $('.button').on('click', '#submit-button', function(e) {
+    $('.button').on('click', '#submit-button', function (e) {
         // Prevent the page from reloading.
         e.preventDefault();
         // Check the response and update the score.
@@ -84,7 +93,9 @@ $(document).ready( function() {
         feedback = judgement + " " + feedback;
         // Hide the list of choices and display the feedback.
         $('ul.response-list').hide();
-        $('div.answer').html('<span>Answer: </span><p class="answer-text">' + feedback + '</p>');
+        $('div.answer').html(
+            '<span>Answer: </span><p class="answer-text">' +
+            feedback + '</p>');
         $('div.answer').show();
         // Set the current image to the actual image.
         setImage('actual');
@@ -99,7 +110,7 @@ $(document).ready( function() {
 
     });
 
-    $('.button').on('click', '#next-button', function(e) {
+    $('.button').on('click', '#next-button', function (e) {
         // Prevent the button event from reloading the page.
         e.preventDefault();
         // Get the new question and answer choices.
@@ -115,7 +126,7 @@ $(document).ready( function() {
 
     });
 
-    $('.button').on('click', '#retake-button', function(e) {
+    $('.button').on('click', '#retake-button', function (e) {
         // Prevent the page reload.
         e.preventDefault();
         startQuiz();
@@ -135,9 +146,12 @@ $(document).ready( function() {
     }
 
     function setButton(value) {
-        var submitHTML = '<input type="submit" id="submit-button" value="Submit" name="submit" />';
-        var nextHTML = '<input type="submit" id="next-button" value="Next" name="next" />';
-        var retakeHTML = '<input type="submit" id="retake-button" value="Try Again" name="retake" />';
+        var submitHTML =
+            '<input type="submit" id="submit-button" value="Submit" name="submit" />';
+        var nextHTML =
+            '<input type="submit" id="next-button" value="Next" name="next" />';
+        var retakeHTML =
+            '<input type="submit" id="retake-button" value="Try Again" name="retake" />';
         if (value === 'submit') {
             $('.button').html(submitHTML);
         } else if (value === 'next') {
@@ -154,12 +168,17 @@ $(document).ready( function() {
         var number = questionNumber + 1;
 
         // Insert the new question text and the list of choices into the page.
-        $('.question').html('<span>Question </span><span class="count">' + number + '</span>: <p class="answer-text">' + questionText + '</p>');
+        $('.question').html(
+            '<span>Question </span><span class="count">' + number +
+            '</span>: <p class="answer-text">' + questionText +
+            '</p>');
         $('ul.response-list').empty();
         $('div.answer').hide();
         $('ul.response-list').show();
-        for (var i=0; i < choices.length; i++) {
-            $('ul.response-list').append('<li class="response"><input type="radio" name="selection" value="' + i + '"><span>' + choices[i] + '</span></li>');
+        for (var i = 0; i < choices.length; i++) {
+            $('ul.response-list').append(
+                '<li class="response"><input type="radio" name="selection" value="' +
+                i + '"><span>' + choices[i] + '</span></li>');
         }
         return;
     }
@@ -167,7 +186,8 @@ $(document).ready( function() {
     function checkResponse() {
         // Add a few variables that will be used to render the correct
         // score ball depending on the user's response to the question.
-        var choice = $("input[type='radio'][name='selection']:checked").val();
+        var choice = $("input[type='radio'][name='selection']:checked")
+            .val();
         var correctAnswer = questions[questionNumber].correct;
         var correct;
         var ball;
@@ -185,8 +205,11 @@ $(document).ready( function() {
 
     function resetScore() {
         $('ul.tennis-balls').empty();
-        for (var i=0; i < questions.length; i++) {
-            $('ul.tennis-balls').append('<li class="score-ball ball-' + i + '"><img src="images/no-answer-ball.png" height="57" width="57" alt="Score ball"></li>');
+        for (var i = 0; i < questions.length; i++) {
+            $('ul.tennis-balls').append('<li class="score-ball ball-' +
+                i +
+                '"><img src="images/no-answer-ball.png" height="57" width="57" alt="Score ball"></li>'
+            );
         }
     }
 
